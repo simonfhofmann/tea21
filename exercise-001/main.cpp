10203cfe13861d38df1a22e759c5e6f05e775555
simonfhofmann <111226422+simonfhofmann@users.noreply.github.com>
2023-11-03 15:04:52 +0100

    added info

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -6,7 +6,14 @@
 
 #include "CLI/CLI.hpp"
 #include "config.h"
+// .bss segment
 int foo;
+// .data segment
+int data = 4711;
+
+// .rodata segment
+int const rodata = 69;
+
 
 auto main(int argc, char **argv) -> int
 {
@@ -17,6 +24,10 @@ auto main(int argc, char **argv) -> int
      * More info at https://fmt.dev/latest/api.html
      */
     fmt::print("Hello, {}!\n", "hello i am under water please help me");
-    fmt::print("Value of foo {} adress of foo{}",foo,fmt::ptr(&foo));
+
+    fmt::print("Value of foo {} adress of foo{}\n",foo,fmt::ptr(&foo));
+    fmt::print("Value of dta {} adress of data{}\n",data,fmt::ptr(&data));
+    fmt::print("Value of rodata {} adress of rodata{}\n",rodata,fmt::ptr(&rodata));
+
     return 0; /* exit gracefully*/
 }
