420b9872d43e1f34344684faf6decab7c4f4483d
simonfhofmann <111226422+simonfhofmann@users.noreply.github.com>
2023-11-10 09:15:01 +0100

    commit for future pull

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -9,10 +9,19 @@
 // .bss segment
 int foo;
 // .data segment
-int data = 4711;
+int data = 2345;
 
 // .rodata segment
 int const rodata = 69;
+//write function that gets users name and prints it out
+auto get_name() -> std::string
+{
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+    return name;
+}
+
 
 
 auto main(int argc, char **argv) -> int
@@ -28,6 +37,9 @@ auto main(int argc, char **argv) -> int
     fmt::print("Value of foo {} adress of foo{}\n",foo,fmt::ptr(&foo));
     fmt::print("Value of dta {} adress of data{}\n",data,fmt::ptr(&data));
     fmt::print("Value of rodata {} adress of rodata{}\n",rodata,fmt::ptr(&rodata));
+
+    std::string name = get_name();
+    fmt::print("Hello, {}!\n", name);
 
     return 0; /* exit gracefully*/
 }
